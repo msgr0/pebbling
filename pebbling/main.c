83c688cb2941f0f71492afce7a4ed123016c1d7d
sgmattia <mttsgro@gmail.com>
2026-01-27 13:04:34 +0100

    Initial Commit

---
+++ pebbling/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  pebbling
+//
+//  Created by Mattia Sgrò on 27/01/26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
